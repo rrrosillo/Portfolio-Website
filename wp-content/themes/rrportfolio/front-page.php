0035eefd5f20098c85a0383378a04a06ca366299
rrrosillo <reuben.rosillo@gmail.com>
2026-04-28 14:53:23 +0800

    Added About Section

--- wp-content/themes/rrportfolio/front-page.php
+++ wp-content/themes/rrportfolio/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php get_template_part('template-parts/sections/hero'); ?>
-<?php // get_template_part('template-parts/sections/features'); ?>
+<?php get_template_part('template-parts/sections/about'); ?>
 <?php // get_template_part('template-parts/sections/properties'); ?>
 <?php // get_template_part('template-parts/sections/testimonials'); ?>
 <?php // get_template_part('template-parts/sections/faq'); ?>
---
+++ wp-content/themes/rrportfolio/template-parts/sections/about.php
@@ -0,0 +1,9 @@
+<section class="about container">
+
+  <div class="title-box">ABOUT ME</div>
+
+  <p class="about-text">
+    Nulla in velit a metus rhoncus tempus...
+  </p>
+
+</section>
