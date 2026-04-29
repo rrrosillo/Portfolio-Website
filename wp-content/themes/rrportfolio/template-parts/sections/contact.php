44b7b1c3b2712c2443ee26bdd11589ee1a4675fa
rrrosillo <reuben.rosillo@gmail.com>
2026-04-29 13:27:19 +0800

    additional structure fix

--- wp-content/themes/rrportfolio/template-parts/sections/contact.php
+++ wp-content/themes/rrportfolio/template-parts/sections/contact.php
@@ -1,34 +1,11 @@
-<section class="featured-properties">
-  <div class="container">
+<section class="contact container">
 
-    <!-- HEADER -->
-    <div class="section-header">
-      <div>
-        <h2>Featured Properties</h2>
-        <p>Explore our handpicked selection of premium properties.</p>
-      </div>
+  <div class="title-box">CONTACT SECTION</div>
 
-      <a href="/properties" class="btn-outline">View All Properties</a>
-    </div>
+  <p class="about-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+  </p>
 
-    <!-- GRID -->
-    <div class="property-grid">
+  <a href="#">EXPLORE</a>
 
-      <?php
-      $query = new WP_Query([
-        'post_type' => 'property',
-        'posts_per_page' => 6
-      ]);
-
-      while($query->have_posts()) : $query->the_post(); ?>
-        
-        <div class="property-grid__item">
-          <?php get_template_part('template-parts/components/property-card', 'property'); ?>
-        </div>
-
-      <?php endwhile; wp_reset_postdata(); ?>
-
-    </div>
-
-  </div>
 </section>
--- wp-content/themes/rrportfolio/template-parts/sections/features.php
+++ wp-content/themes/rrportfolio/template-parts/sections/features.php
@@ -1,25 +1,11 @@
-<section class="features">
-  <div class="container features-grid">
+<section class="features container">
 
-    <div class="feature-card">
-      <div class="icon"><img src="/growmodo/wp-content/uploads/2026/04/find-dream-home-icon.png" alt="" class="wp-image-23"/></div>
-      <h3>Find Your Dream Home</h3>
-    </div>
+  <div class="title-box">FEATURES SECTION</div>
 
-    <div class="feature-card">
-      <div class="icon"><img src="/growmodo/wp-content/uploads/2026/04/unlock-property-value-icon.png" alt="" class="wp-image-23"/></div>
-      <h3>Unlock Property Value</h3>
-    </div>
+  <p class="about-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+  </p>
 
-    <div class="feature-card">
-      <div class="icon"><img src="/growmodo/wp-content/uploads/2026/04/effortless-property-management-icon.png" alt="" class="wp-image-23"/></div>
-      <h3>Effortless Property Management</h3>
-    </div>
+  <a href="#">EXPLORE</a>
 
-    <div class="feature-card">
-      <div class="icon"><img src="/growmodo/wp-content/uploads/2026/04/smart-investments-icon.png" alt="" class="wp-image-23"/></div>
-      <h3>Smart Investments</h3>
-    </div>
-
-  </div>
 </section>
